099bf2ca9c6b1cf226bd86f7dc418343ecc1c6e7
nk0086 <n172586k@gmail.com>
2025-06-08 19:36:21 +0900

    feat: settings of cli tools for macOS

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Minimal package definition for aarch64-darwin";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    neovim-nightly-overlay,
+  }: let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system}.extend (
+      neovim-nightly-overlay.overlays.default
+    );
+  in {
+    packages.${system}.my-packages = pkgs.buildEnv {
+      name = "my-packages-list";
+      paths = with pkgs; [
+        git
+        curl
+        alejandra
+        neovim
+      ];
+    };
+
+    # nix run .#update
+    apps.${system}.update = {
+      type = "app";
+      program = toString (pkgs.writeShellScript "update-script" ''
+        set -e
+        echo "Updating flake ... "
+        nix flake update
+        echo "Updating profile ... "
+        nix profile upgrade my-packages
+        echo "Update complete!"
+      '');
+    };
+  };
+}
