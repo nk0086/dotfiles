1d7e89168b31dd00f7de924df60e4273692ecb9d
nk0086 <n172586k@gmail.com>
2025-06-15 17:54:04 +0900

    refactor: format

--- config/nvim/config/plugins.lua
+++ config/nvim/config/plugins.lua
@@ -1,58 +1,58 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "nvim-lua/plenary.nvim" },
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-},
-    { "machakann/vim-sandwich" },
-    { "rhysd/clever-f.vim" },
-    { "iberianpig/tig-explorer.vim" },
-    { "cocopon/iceberg.vim" },
-    { "tpope/vim-commentary" },
-    { "neovim/nvim-lspconfig" },
-    {
-        "stevearc/conform.nvim",
-        opts = {},
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "rust", "go", "python", "nim", "nix", "lua", "typescript", "javascript", "gleam"
-                },
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true,
-                },
-            })
-        end,
-    },
+	{ "nvim-lua/plenary.nvim" },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
+	{ "machakann/vim-sandwich" },
+	{ "rhysd/clever-f.vim" },
+	{ "iberianpig/tig-explorer.vim" },
+	{ "cocopon/iceberg.vim" },
+	{ "tpope/vim-commentary" },
+	{ "neovim/nvim-lspconfig" },
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	-- {
+	--     "nvim-treesitter/nvim-treesitter",
+	--     build = ":TSUpdate",
+	--     config = function()
+	--         require("nvim-treesitter.configs").setup({
+	--             ensure_installed = {
+	--                 "rust", "go", "python", "nim", "nix", "lua", "typescript", "javascript", "gleam"
+	--             },
+	--             auto_install = false,
+	--             highlight = {
+	--                 enable = true,
+	--             },
+	--             indent = {
+	--                 enable = true,
+	--             },
+	--         })
+	--     end,
+	-- },
 })
