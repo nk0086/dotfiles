1308b998ceebeca2775415892cd2207c202a0e90
nk0086 <n172586k@gmail.com>
2025-06-17 22:37:09 +0900

    refactor: move nvim-tree

--- config/nvim/config/options.lua
+++ config/nvim/config/options.lua
@@ -10,6 +10,4 @@ vim.opt.list = true
 vim.opt.listchars = { tab = ">-", trail = "-" }
 vim.opt.cursorline = true
 vim.opt.completeopt = "menuone"
-vim.opt.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
-
