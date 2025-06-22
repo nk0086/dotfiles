ef81685c25a73323ac836029189d26fcc96f7be9
nk0086 <n172586k@gmail.com>
2025-06-22 18:11:47 +0900

    feat: Enable Copilot for specific filetypes (markdown, gitcommit, yaml)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,12 @@
 local config_path = vim.fn.stdpath("config")
 package.path = package.path .. ";" .. config_path .. "/?.lua;" .. config_path .. "/plugins/?.lua"
 
+vim.g.copilot_filetypes = {
+	markdown = true,
+	gitcommit = true,
+	yaml = true,
+}
+
 require("plugins.plugins")
 require("plugins.lualine")
 require("plugins.nvim-tree")
