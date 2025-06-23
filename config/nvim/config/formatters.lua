a7455b3716c6931b1812ca1ffce2443c4b4b448c
nk0086 <n172586k@gmail.com>
2025-06-23 22:01:37 +0900

    chore: fmt

--- config/nvim/config/formatters.lua
+++ config/nvim/config/formatters.lua
@@ -1,20 +1,20 @@
 require("conform").setup({
-  formatters_by_ft = {
-    rust = { "rustfmt" },
-    go = { "gofmt", "goimports" },
-    python = { "black", "ruff_format" },
-    nim = { "nimpretty" },
-    nix = { "alejandra" },
-    lua = { "stylua" },
-    typescript = { "prettier" },
-    javascript = { "prettier" },
-    json = { "prettier" },
-    yaml = { "prettier" },
-    markdown = { "prettier" },
-    gleam = { "gleam" },
-  },
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+    formatters_by_ft = {
+        rust = { "rustfmt" },
+        go = { "gofmt", "goimports" },
+        python = { "black", "ruff_format" },
+        nim = { "nimpretty" },
+        nix = { "alejandra" },
+        lua = { "stylua" },
+        typescript = { "prettier" },
+        javascript = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        gleam = { "gleam" },
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
 })
